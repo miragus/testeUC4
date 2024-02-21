4262e0f7dc484c7980f0ee9da26683e34078a2dd
Rhuan Gustavo Miranda <153655600+miragus@users.noreply.github.com>
2024-02-21 17:24:11 -0300

    21/02/24

--- JS.v1/src/ts/exercicio1.ts
+++ JS.v1/src/ts/exercicio1.ts
@@ -1,30 +1,59 @@
-class Funcionario {
+class Pessoa {
     nome: string;
+    endereco: string[];
+    cpf: string;
+    rg: string;
+    data_nascimento: Date;
     email: string;
     telefone: string;
-    cargo: string;
-    data: Date;
-    constructor(nome: string, email: string, telefone: string, cargo: string, data: Date) {
-        this.cargo = cargo,
+    constructor(nome: string, endereco: string[], cpf: string, rg: string, data_nascimento: Date, email: string, telefone: string) {
+        this.nome = nome,
+            this.endereco = endereco,
+            this.cpf = cpf,
+            this.rg = rg,
+            this.data_nascimento = data_nascimento,
             this.email = email,
-            this.telefone = telefone,
-            this.nome = nome,
-            this.data = data
+            this.telefone = telefone
     }
+    //Método para mostrar os dados
     mostrarDados() {
-        console.log('Dados do funcionário:')
+        console.log('Dados da pessoa:')
         console.log(`Nome: ${this.nome}`)
+        console.log(`Endereço: ${this.endereco}`)
+        console.log(`CPF: ${this.cpf}`)
+        console.log(`RG: ${this.rg}`)
+        console.log(`Data de nascimento: ${this.data_nascimento.toLocaleDateString('pt-BR')}`)
         console.log(`E-mail: ${this.email}`)
         console.log(`Telefone: ${this.telefone}`)
+    }
+
+}
+
+class Funcionario extends Pessoa {
+    //atributos próprios da classe funcionário
+    cargo: string;
+    matricula: string;
+
+
+    //método construtor da classe
+    constructor(nome: string, email: string, telefone: string, cargo: string, data_nascimento: Date, matricula: string, cpf: string, rg: string, endereco: string[]) {
+        super(nome, endereco, cpf, rg, data_nascimento, email, telefone)
+        this.cargo = cargo;
+        this.matricula = matricula;
+    }
+
+    mostrarDados1() {
+        super.mostrarDados()
         console.log(`Cargo: ${this.cargo}`)
-        console.log(`Data de Nascimento: ${this.data.toLocaleString('pt-BR')}`)
+        console.log(`Matrícula: ${this.matricula}`)
+
     }
 }
 
 
-const funcionario1 = new Funcionario('Gustavo', 'gusta@gmail.com', '1234567890', 'Médico', new Date('1990-5-13'))
+const funcionario1 = new Funcionario('Gustavo', 'gusta@gmail.com', '1234567890', 'Médico', new Date('1990-5-13'), '123456789', '123456789', '154899532', ['rua verde'])
 
-console.log(funcionario1.mostrarDados())
+console.log(funcionario1.mostrarDados1())
 
 
 
@@ -53,28 +82,18 @@ console.log(consulta1.mostrarDados2())
 
 //
 
-class Paciente {
-    nome: string;
-    idade: string;
-    cpf: string;
-    telefone: string;
+class Paciente extends Pessoa {
     convenio: boolean;
-    constructor(nome: string, idade: string, cpf: string, telefone: string, convenio: boolean) {
-        this.nome = nome,
-            this.idade = idade,
-            this.cpf = cpf,
-            this.telefone = telefone,
-            this.convenio = convenio
+    constructor(nome: string, cpf: string, telefone: string, convenio: boolean, email: string, rg: string, data_nascimento: Date, endereco: string[]) {
+        super(nome, endereco, cpf, rg, data_nascimento, email, telefone)
+        this.convenio = convenio
     }
-    mostrarDados3(){
+    mostrarDados3() {
+        super.mostrarDados()
         console.log('Dados do paciente:')
-        console.log(`Nome: ${this.nome}`)
-        console.log(`Idade: ${this.idade}`)
-        console.log(`CPF: ${this.cpf}`)
-        console.log(`Telefone: ${this.telefone}`)
         console.log(`Convenio: ${this.convenio}`)
     }
 }
-const paciente1 = new Paciente('Pedro', '18', '1234567890', '0987654321', true)
+const paciente1 = new Paciente('Pedro', '1234567890', '0987654321', true, 'pedro@gmail.com', '987456321', new Date('2000-5-12'), ['Rua vermelha'])
 
 console.log(paciente1.mostrarDados3())
